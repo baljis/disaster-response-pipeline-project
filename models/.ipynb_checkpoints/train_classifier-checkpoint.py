410e310de8f4e9a582f65a412639e80fb75c054b
baljis <singh16.baljeet@gmail.com>
2022-03-03 12:14:00 +0530

    resolved commits

--- models/.ipynb_checkpoints/train_classifier-checkpoint.py
+++ models/.ipynb_checkpoints/train_classifier-checkpoint.py
@@ -1,24 +1,67 @@
 import sys
+import re
+import pandas as pd
+import numpy as np
+import nltk
+import pickle
+import sqlite3 as sq
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
 
+def partition_msg_cat(df):
+    messages = df.message
+    categories = df[df.columns[3:]]
+    
+    return messages,categories
 
 def load_data(database_filepath):
-    pass
+    db = sq.connect(database_filepath)
+    df = pd.read_sql("SELECT * FROM messages_categories",db)
+    X, Y = partition_msg_cat(df)
+    categories = Y.columns
+    return X,Y,categories
 
+stop_words = stopwords.words('english')
+lemmatizer = WordNetLemmatizer()
 
 def tokenize(text):
-    pass
-
+    text = re.sub(r'[^a-zA-Z0-9]'," ",text)
+    
+    tokens = word_tokenize(text.lower())
+    
+    stem_tk = [lemmatizer.lemmatize(token) for token in tokens if token not in stop_words]
+    
+    return stem_tk
 
 def build_model():
-    pass
+    vect = CountVectorizer(tokenizer = tokenize)
+    tfidf = TfidfTransformer(smooth_idf=False)
+    claf = RandomForestClassifier()
+    
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer(smooth_idf=False)),
+        ('claf', RandomForestClassifier())
+        ])
+    
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+    Y_pred = model.predict(X_test)
+    accuracy = ((Y_test == Y_pred).mean()).mean()
+    print('Accuracy of Model is : {}'.format(accuracy))
 
 def save_model(model, model_filepath):
-    pass
+    try:
+        pickle.dump(model,open(model_filepath,'wb'))
+    except:
+        print("Failed dumping")
 
 
 def main():
